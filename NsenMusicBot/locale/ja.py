4a79581613e797cc83264fffe1dd1de0b5256511
Nep <spica@starry.blue>
2016-11-16 23:54:25 +0900

    Delete: unused message

--- NsenMusicBot/locale/ja.py
+++ NsenMusicBot/locale/ja.py
@@ -3,6 +3,4 @@
 logFormat = "[%(asctime)s][%(threadName)s %(name)s/%(levelname)s]: %(message)s"
 logTimeFormat = "%H:%M:%S"
 
-textNotPlayingAnything = "現在なにも再生されていません。"
-textPlaying = "Now Playing: {songTitle} [Skip Votes: {voteCount}/3]"
-
+textPlaying = "Now Playing: {songTitle}"
