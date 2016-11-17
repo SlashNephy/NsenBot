c05e8bda385f14a25c648eed61d5c525636ba0f4
Nep <spica@starry.blue>
2016-11-17 20:38:10 +0900

    Fix: async-await

--- NsenMusicBot/main.py
+++ NsenMusicBot/main.py
@@ -1,64 +1,114 @@
 # coding=utf-8
-import time
-import datetime
 import asyncio
+import datetime
+import time
+import threading
 
 import discord
 
-if not discord.opus.is_loaded():
-	discord.opus.load_opus("opus")
-
-from NsenMusicBot.utils import getCustomLogger
-from NsenMusicBot.nsen import Nsen
-from NsenMusicBot.configparser import ConfigParser
+from .utils import getCustomLogger
+from .nsen import Nsen
+from .configparser import ConfigParser
 
 class Bot:
-	def __init__(self, debug=False):
-		configparser = ConfigParser()
-		self.config = configparser.config
+	def __init__(self):
+		self.running = False
 
-		self.logger = getCustomLogger("{}/{}.log".format(self.config["logDir"], datetime.datetime.now().strftime("%Y%m%d_%H%M%S")), debug)
+		if not discord.opus.is_loaded():
+			discord.opus.load_opus("opus")
 
-		self.bot = discord.Client()
+		self.config = ConfigParser().config
+
+		self.isDebugMode = self.config.get("debug", False)
+		self.logger = getCustomLogger("{}/{}.log".format(self.config["logDir"], datetime.datetime.now().strftime("%Y%m%d_%H%M%S")), self.isDebugMode)
+
+		self.client = discord.Client()
+		self.voiceClient = None
+		self.volume = 0.05
 		self.voiceChannel = discord.Object(id=self.config["bot"]["channel"])
 		self.textChannel = discord.Object(id=self.config["bot"]["textChannel"])
-		self.volume = 0.3
+
+		self.nsenThread = threading.Thread(name="Sync Nsen Song", target=self.syncNsen)
+		self.nsenThread.setDaemon(True)
+		self.musicQueue = []
 
 		self.nsen = Nsen(self.config["niconico"]["default"])
 		self.nsen.login(self.config["niconico"]["email"], self.config["niconico"]["password"])
 
-		self.tmpDir = self.config["tmpDir"]
-
-		@self.bot.event
+		@self.client.event
 		async def on_ready():
-			print("Logged in as: {0} (ID: {0.id})".format(self.bot.user))
+			self.logger.info("Logged in as: {0} (ID: {0.id})".format(self.client.user))
 
-			await self.playNsen()
+			self.voiceClient = await self.client.join_voice_channel(self.voiceChannel)
+			await self.loopTrack()
 
-	async def playNsen(self):
-		voice = await self.bot.join_voice_channel(self.voiceChannel)
+	async def loopTrack(self):
+		while self.running:
+			await self.goNextTrack()
+
+	async def goNextTrack(self):
+		track = await self.getTrack()
+		try:
+			player = self.voiceClient.create_ffmpeg_player(track["path"], use_avconv=True)
+			player.volume = self.volume
+			player.start()
+
+			await self.client.send_message(self.textChannel, track["text"])
+			self.logger.info(track["text"])
+
+			n = 1
+			while True:
+				self.logger.debug("Sleeping {} secs".format(n))
+				await asyncio.sleep(1)
+				n += 1
+				if player.is_done():
+					self.logger.debug("Sleeping is done.")
+					break
+
+			self.logger.debug("Current song is done.")
+			await self.goNextTrack()
+		except:
+			self.logger.exception("Error occured while goNextTrack")
+			await asyncio.sleep(10)
+
+	async def getTrack(self):
+		while len(self.musicQueue) == 0:
+			await asyncio.sleep(1)
+		return self.musicQueue.pop(0)
+
+	def syncNsen(self):
+		previousId = None
 		while True:
 			try:
 				self.nsen.getPlayerStatus()
-				duration = int(self.nsen.liveData["stream"]["contents_list"]["contents"]["@duration"])
-				text = "Now Playing: {title} ({time}) - {channelName}\n{channelDesc}".format(
-						title=self.nsen.liveData["stream"]["contents_list"]["contents"]["@title"],
-						time="{}:{:02d}".format(duration / 60, duration % 60),
-						channelName=self.nsen.liveData["stream"]["title"],
-						channelDesc=self.nsen.liveData["stream"]["description"]
-				)
-				await self.bot.send_message(self.textChannel, text)
+				while previousId == self.nsen.data["id"]:
+					time.sleep(1)
 				self.nsen.getCKey()
 				self.nsen.getFLV()
-				path = "{}.flv".format(datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
-				self.nsen.generateCommand(path=path)
-				self.nsen.executeRecordCommand()
-				player = voice.create_ffmpeg_player(path)
-				player.volume = self.volume
-				await player.start()
+
+				duration = int(self.nsen.data["live"]["stream"]["contents_list"]["contents"]["@duration"])
+				obj = {
+					"path": "{}/{}.flv".format(self.config["tmpDir"], datetime.datetime.now().strftime("%Y%m%d_%H%M%S")),
+					"text": "Now Playing: {title} ({time}) - {channelName}".format(
+						title=self.nsen.data["live"]["stream"]["contents_list"]["contents"]["@title"],
+						time="{0[0]}:{0[1]:02d}".format(divmod(duration, 60)),
+						channelName=self.nsen.data["live"]["stream"]["title"]
+					)
+				}
+				previousId = self.nsen.data["id"]
+
+				self.nsen.generateCommand(path=obj["path"])
+				result = self.nsen.executeRecordCommand()
+				self.logger.info("Record command result =\n{0[0]}\n\nError =\n{0[1]}".format(result[0]))
+				self.musicQueue.append(obj)
 			except:
-				self.logger.exception("Error occured while playNsen")
-				await asyncio.sleep(10)
+				self.logger.exception("Error occured while syncNsen")
+				time.sleep(5)
+			finally:
+				self.nsen.initializeLive()
 
 	def run(self):
-		self.bot.run(self.config["bot"]["token"])
+		self.running = True
+
+		self.nsenThread.start()
+		self.client.run(self.config["bot"]["token"])
